d65829b4e7fd932bbe4f81bc1b89368edbc2c752
anatoliipiddubnyi <poddubny.anatoly@gmail.com>
2025-03-26 10:45:15 +0200

    add other elements

---
+++ src/main/java/com/udemy/seleniumdesign/srp/ResultStat.java
@@ -0,0 +1,25 @@
+package com.udemy.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
+public class ResultStat extends AbstractComponent {
+    @FindBy(id = "resultStat")
+    private WebElement resultStat;
+
+
+    public ResultStat(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getStat() {
+        return resultStat.getText();
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return wait.until(_ -> resultStat.isDisplayed());
+    }
+}
