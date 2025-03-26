f60653ccf15ad5d5cbefc27aef3689274fd20a75
anatoliipiddubnyi <poddubny.anatoly@gmail.com>
2025-03-26 10:31:36 +0200

    refactoring

--- src/main/java/com/udemy/seleniumdesign/srp/AbstractComponent.java
+++ src/main/java/com/udemy/seleniumdesign/srp/AbstractComponent.java
@@ -1,5 +1,17 @@
 package com.udemy.seleniumdesign.srp;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public abstract class AbstractComponent {
+    protected WebDriverWait wait;
+
+    public AbstractComponent(final WebDriver driver) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        PageFactory.initElements(driver, this);
+    }
     public abstract boolean isDisplayed();
 }
--- src/main/java/com/udemy/seleniumdesign/srp/SearchWidget.java
+++ src/main/java/com/udemy/seleniumdesign/srp/SearchWidget.java
@@ -3,23 +3,14 @@ package com.udemy.seleniumdesign.srp;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 public class SearchWidget extends AbstractComponent{
 
-    private WebDriverWait wait;
-    
     @FindBy(name="q")
     private WebElement searchBox;
 
     public SearchWidget(final WebDriver driver) {
-        wait = new WebDriverWait(driver, Duration.of(30, ChronoUnit.SECONDS));
-        PageFactory.initElements(driver, this);
+        super(driver);
 
     }
 
@@ -31,7 +22,6 @@ public class SearchWidget extends AbstractComponent{
 
     @Override
     public boolean isDisplayed() {
-        wait.until(ExpectedConditions.visibilityOf(searchBox));
-        return searchBox.isDisplayed();
+        return wait.until(_ -> searchBox.isDisplayed());
     }
 }
